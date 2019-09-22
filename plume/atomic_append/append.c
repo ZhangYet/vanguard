d1da8b36a45d84cc71987d9fa5d988136e8dc2f0
zhangye <zhangyet@gmail.com>
2019-09-22 20:47:28 +0800

    atomic append

---
+++ plume/atomic_append/append.c
@@ -0,0 +1,48 @@
+//
+// Created by dantezy on 2019/9/22.
+//
+
+#include <fcntl.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    bool is_append = true;
+    printf("argc: %d, argv[3]: %s\n", argc, argv[3]);
+    if (argc > 3 && strcmp(argv[3], "x") == 0) {
+        is_append = false;
+    }
+    char *to_write = "x";
+    if (argc > 4) {
+        to_write = argv[4];
+    }
+    int byte_to_write = atoi(argv[2]);
+
+    int fd;
+    if (is_append) {
+        printf("is append\n");
+        fd = open(argv[1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+    } else {
+        fd = open(argv[1], O_WRONLY | O_CREAT, 0644);
+    }
+    if (fd == -1) {
+        printf("cannot open file %s\n", argv[1]);
+        exit(-1);
+    } else {
+        printf("fd: %d\n", fd);
+    }
+
+    for (int i = 0; i < byte_to_write; i++) {
+        int written = write(fd, to_write, strlen(to_write));
+        if (written == -1) {
+            printf("write failed\n");
+            exit(-1);
+        }
+    }
+
+    close(fd);
+    exit(0);
+}
