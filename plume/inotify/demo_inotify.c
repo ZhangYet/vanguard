0fb0679ccc237da4bfe52d5f4f6605be59ad165d
dantezy <zhangyet@gmail.com>
2019-10-10 22:00:32 +0800

    update

--- plume/inotify/demo_inotify.c
+++ plume/inotify/demo_inotify.c
@@ -1,4 +1,25 @@
 #include <sys/inotify.h>
 #include <limits.h>
+#include <stdio.h>
 
+static void displayInotifyEvent(struct inotify_event *i) {
+  printf("    wd =%2d; ", i->wd);
+  if (i->cookie > 0)
+    printf("cookie =%4d;", i->cookie);
 
+  printf("mask = ");
+  if (i->mask & IN_ACCESS) printf("IN_ACCESS");
+  if (i->mask & IN_ATTRIB) printf("IN_ATTRIB");
+  print("\n");
+
+  if (i->len > 0)
+    printf("    name = %s\n", i->name);
+}
+
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + 256 + 1))
+
+int main(int argc, char *argv[]) {
+  int inotifyFd, wd, j;
+  char buf[BUF_LEN];
+
+}
