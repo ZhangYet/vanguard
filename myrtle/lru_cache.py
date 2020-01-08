e6147c8d51f78c6089d416f1d5cecdc6694e26be
dante zhang <zhangye@16financial.com>
2020-01-08 14:30:11 +0800

    add lru_cache to do

---
+++ myrtle/lru_cache.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/lru-cache/
+
+# 之前的做法是用 deque 做的，实际的复杂度还是 O(1)
+# 尝试自己用链表做
+
+# 答案：https://leetcode.com/problems/lru-cache/discuss/467267/Python3-Video-Explanation-O(1)-solution-(
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        pass
+
+    def get(self, key: int) -> int:
+        return -1
+
+    def put(self, key: int, value: int):
+        return
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
