9e2ecf4c4a1adb3943d62506a052274dcf1d87ee
dantezy <zhangyet@gmail.com>
2020-01-08 18:50:13 +0800

    一个错误的版本

---
+++ myrtle/reverse_linked_list.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def ListNodeIterator(node: ListNode):
+    while True:
+        if not node:
+            return
+        yield node.val
+        node = node.next
+
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        def reverse(h: ListNode) -> ListNode:
+            print(h.val)
+            if not h.next:
+                return h
+
+            tail = reverse(h.next)
+            tail.next = h
+            return h
+
+        tail = head
+        while tail.next:
+            tail = tail.next
+        reverse(head)
+        return tail
+
+
+def gen_case(n: int):
+    n0 = ListNode(0)
+    curr = n0
+    for i in range(1, n):
+        n = ListNode(i)
+        curr.next = n
+        curr = n
+    return n0
+
+
+def test_case():
+    curr = gen_case(4)
+    t = Solution()
+    return t.reverseList(curr)
