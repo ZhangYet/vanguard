fbc27046964b234e9c4c9390cb35aa82a8aaf2d8
dantezy <zhangyet@gmail.com>
2020-01-12 15:18:12 +0800

    尝试了中序遍历和后序遍历

--- myrtle/subtree_of_another_tree.py
+++ myrtle/subtree_of_another_tree.py
@@ -17,6 +17,20 @@ class Solution:
 
             return f'{_hash(node.val)}{preorder(node.left)}{preorder(node.right)}'
 
-        s_str = preorder(s)
-        t_str = preorder(t)
-        return t in s_str
+        def inorder(node: TreeNode) -> str:
+            if not node:
+                return '#'
+
+            return f'{inorder(node.left)}{_hash(node.val)}{inorder(node.right)}'
+
+        def postorder(node: TreeNode) -> str:
+            if not node:
+                return '#'
+
+            return f'{postorder(node.left)}{postorder(node.right)}{_hash(node.val)}'
+
+        s_str = postorder(s)
+        t_str = postorder(t)
+        return t_str in s_str
+
+# 只有 inorder 是不行的，奇怪
