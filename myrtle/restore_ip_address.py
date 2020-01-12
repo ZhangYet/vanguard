7773b25c05be836b185e3a01d6c3a43449a176d0
zhangye <zhangyet@gmail.com>
2020-01-12 13:53:04 +0800

    update

--- myrtle/restore_ip_address.py
+++ myrtle/restore_ip_address.py
@@ -3,20 +3,16 @@
 from typing import List
 
 
-def lstrim_zero(s: str):
-    tmp = s.lstrip('0')
-    if len(tmp) > 0:
-        return tmp
-    return '0'
-
 def is_valid_sec(s: str):
     if not s:
         return False
-    try:
-        return 0 <= int(s) and int(s) <= 255
-    except ValueError:
-        print(f'parse error: {s}')
+    if s == '0':
+        return True
+    if s.startswith('0'):
         return False
+
+    return 1 <= int(s) and int(s) <= 255
+
 
 
 class Solution:
@@ -25,27 +21,23 @@ class Solution:
 
         def parse(s: str, n: int) -> List[str]:
             if n == 1:
-                clean_s = lstrim_zero(s)
-                print(f'the last session: {s}, {clean_s}')
-                if is_valid_sec(clean_s):
-                    return [clean_s]
+                if is_valid_sec(s):
+                    return [s]
                 else:
                     return []
             res = []
             if is_valid_sec(s[:1]):
-                res += [s[:1] + '.' + x for x in parse(s[1:], n-1)]
+                res += [f'{s[:1]}.{x}' for x in parse(s[1:], n-1)]
 
-            clean_h2 = lstrim_zero(s[:2])
-            if is_valid_sec(clean_h2):
-                res += [clean_h2 + '.' + x for x in parse(s[2:], n-1)]
+            if is_valid_sec(s[:2]):
+                res += [f'{s[:2]}.{x}' for x in parse(s[2:], n-1)]
 
-            clean_h3 = lstrim_zero(s[:3])
-            if is_valid_sec(clean_h3):
-                res += [clean_h3 + '.' + x for x in parse(s[3:], n-1)]
+            if is_valid_sec(s[:3]):
+                res += [f'{s[:3]}.{x}' for x in parse(s[3:], n-1)]
 
             return res
 
-        return list(set(parse(s, 4)))
+        return parse(s, 4)
 
 
 def test_case(s: str):
