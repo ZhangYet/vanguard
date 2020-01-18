92b5bc61228d54b3773df5c18e71fba8ce01b8f3
zhangye <zhangyet@gmail.com>
2020-01-18 16:01:14 +0800

    递归会好做一点

--- myrtle/add_digits.py
+++ myrtle/add_digits.py
@@ -1,1 +1,17 @@
 # https://leetcode.com/problems/add-digits/
+
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        def _add_digit(num: int):
+            if num < 10:
+                return num
+
+            return _add_digit(_add_digit(num // 10) + num % 10)
+
+        return _add_digit(num)
+
+
+def test_case(i: int) -> int:
+    s = Solution()
+    return s.addDigits(i)
