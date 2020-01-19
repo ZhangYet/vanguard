6ac957ab401c036692c940f4dded06a8701cf466
dantezy <zhangyet@gmail.com>
2020-01-19 13:58:49 +0800

    终于做出来了

--- myrtle/next_greater_number_ii.py
+++ myrtle/next_greater_number_ii.py
@@ -3,23 +3,45 @@ from typing import List
 
 # 这样直接找会超时
 # 别以为耍这种小把戏就可以过关！
+# 用 stack!
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         if not nums:
             return []
-
         l = len(nums)
-        res = [0] * l
-        for i in range(l):
-            new_nums = nums[(i+1):] + nums[:i]
-            r = [x for x in new_nums if x > nums[i]]
-            if not r:
-                res[i] = -1
-            else:
-                res[i] = r[0]
+        L = 2 * l
+        res = [-1] * l
+        stack = []
+
+        cur = L - 1
+        while cur >= 0:
+            next_greater_num = -1 if not stack else nums[stack[-1]]
+            index = cur if cur < l else cur - l
+
+
+            if not stack:
+                stack.append(index)
+                cur -= 1
+                continue
+
+            if nums[stack[-1]] > nums[index]:
+                res[index] = nums[stack[-1]]
+                stack.append(index)
+                cur -= 1
+                continue
+
+            while stack:
+                if nums[stack[-1]] <= nums[index]:
+                    stack.pop()
+                else:
+                    res[index] = nums[stack[-1]]
+                    break
+
+            stack.append(index)
+            cur -= 1
 
         return res
 
-def test_case(l: List[int]) -> List[int]:
+def test_case(l: List[int]):
     s = Solution()
     return s.nextGreaterElements(l)
