9fc87812275d8ffdbf7d84fc1aa56998cec61764
dantezy <zhangyet@gmail.com>
2020-02-09 10:13:30 +0800

    add find_bottome_left_tree_value
    其实我觉得这道题目本身有问题

---
+++ myrtle/find_bottom_left_tree_value.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        from collections import deque
+
+        left_children = [root.val]
+        stack = deque([root])
+
+        while stack:
+            cur = stack.popleft()
+            if cur.right:
+                stack.append(cur.right)
+                left_children.append(cur.right.val)
+            if cur.left:
+                left_children.append(cur.left.val)
+                stack.append(cur.left)
+
+        return left_children[-1]
