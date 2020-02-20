6001b59172a99aae083b58ac17007a1ce103b3f3
dante zhang <zhangye@16financial.com>
2020-02-20 21:44:43 +0800

    动态规划好啊

---
+++ myrtle/divisor_game.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/divisor-game/
+from typing import List
+
+
+class Solution:
+    def get_factors(self, N: int) -> List[int]:
+        import math
+
+        res = [1]
+        for i in range(2, round(math.sqrt(N))):
+            if N % i == 0:
+                res.append(i)
+        return res
+
+    def divisorGame(self, N: int) -> bool:
+        if N == 1:
+            return False
+        if N == 2:
+            return True
+        if N == 3:
+            return False
+
+        game_result = [False] * (N + 1)
+        game_result[1] = False
+        game_result[2] = True
+        game_result[3] = False
+
+        for i in range(4, N + 1):
+            factors = self.get_factors(i)
+            for factor in factors:
+                if (factor % 2 == 1) and not game_result[i - factor]:
+                    game_result[i] = True
+                    break
+                if (factor % 2 == 0) and game_result[i - factor]:
+                    game_result[i] = True
+                    break
+        return game_result[N]
+
+
+def test_case(i: int, ex: bool):
+    s = Solution()
+    assert s.divisorGame(i) == ex
