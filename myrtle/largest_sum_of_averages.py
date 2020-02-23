103dc559c83a62ff6eab1d41f22f497cf8d71dc7
dantezy <zhangyet@gmail.com>
2020-02-23 11:06:57 +0800

    居然能用 DP

---
+++ myrtle/largest_sum_of_averages.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/largest-sum-of-averages/
+from typing import List
+
+
+class Solution:
+    def largestSumOfAverages(self, A: List[int], K: int) -> float:
+        pass
