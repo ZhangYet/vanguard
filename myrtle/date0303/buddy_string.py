7cc713b70fe526a47f6dcb2ff09eb6cf5586a893
dantezy <zhangyet@gmail.com>
2020-03-03 16:45:51 +0800

    an ugly answer

---
+++ myrtle/date0303/buddy_string.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/buddy-strings/
+# 感觉这道题是 https://leetcode.com/problems/k-similar-strings/ 的基础，所以做一下
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        len_a = len(A)
+        len_b = len(B)
+
+        if len_a != len_b:
+            return False
+
+        if len_a <= 1:
+            return False
+
+        from collections import Counter
+
+        counter_a = Counter(A)
+        counter_b = Counter(B)
+        if counter_a != counter_b:
+            return False
+
+        diff = 0
+        for i in range(len_a):
+            if B[i] not in counter_a:
+                return False
+            if B[i] != A[i]:
+                diff += 1
+                if diff > 2:
+                    return False
+
+        if diff == 2:
+            return True
+
+        if diff == 0:
+            for k, v in counter_a.items():
+                if v > 1:
+                    return True
+
+            return False
+
+        return False
