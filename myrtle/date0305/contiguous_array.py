a3ada6c96314a298fa5ef70157190079acbb6588
dantezy <zhangyet@gmail.com>
2020-03-06 14:29:30 +0800

    真是不堪

--- myrtle/date0305/contiguous_array.py
+++ myrtle/date0305/contiguous_array.py
@@ -4,44 +4,23 @@
 # 主要是怎样判断我们找到最大了？
 # 怎样处理那些前面有太多0的情况？
 # 试试用类似 DP 的算法吧
+# 看了这个答案： https://leetcode.com/problems/contiguous-array/discuss/485448/Python-3-Presum-%2B-hashtable-O(n)-clean-code-beats-93
+# 真是妙啊，每一位如果是1就加1，如果是0就加-1，记录每种和初次出现的 index，下次再遇到同样的和
+# 把当前的 index 减去第一次出现这种和的 index，中间就是0，1相等的子数组
+# 注意出现0的情况
 from typing import List
 
 
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        matrix = [[] for _ in nums]
-        for row in range(len(nums)):
-            for col in range(len(nums)):
-                if row > col:
-                    matrix[row].append(())
-                    continue
-
-                if row == 0:
-                    matrix[row].append(
-                        (
-                            sum(nums[row : (col + 1)]),
-                            col - row + 1 - sum(nums[row : col + 1]),
-                        )
-                    )
-                    continue
-
-                up_level = matrix[row - 1][col]
-                if nums[row - 1] == 1:
-                    matrix[row].append((up_level[0] - 1, up_level[1]))
-                else:
-                    matrix[row].append((up_level[0], up_level[1] - 1))
-
-        res = 0
-        for row in range(len(nums)):
-            for col in range(len(nums)):
-                if not matrix[row][col]:
-                    continue
-                data = matrix[row][col]
-                if data[1] == data[0]:
-                    res = max(res, col - row + 1)
-        from utils import print_matrix
-
-        print_matrix(matrix)
+        res, s = 0, 0
+        seen = {0: 0}
+        for i, n in enumerate(nums):
+            s = s + 1 if n == 1 else s - 1
+            if s in seen:
+                res = max(res, i - seen[s] if s != 0 else i + 1)
+            else:
+                seen[s] = i
         return res
 
 
