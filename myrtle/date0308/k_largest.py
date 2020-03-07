ee821b000a5f10222faea7be4d10df49cfa7fcc7
dantezy <zhangyet@gmail.com>
2020-03-08 07:45:58 +0800

    过几天复习的时候再做吧

---
+++ myrtle/date0308/k_largest.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/kth-largest-element-in-a-stream/
+# 关键是建一个有容量的堆，而且要记住的是，这个堆不需要保留全部数据
+from typing import List
+import heapq
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        pass
+
+    def add(self, val: int) -> int:
+        pass
