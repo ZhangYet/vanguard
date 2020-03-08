63f7989d5c238ecc250cbe4112650d1343764b67
dantezy <zhangyet@gmail.com>
2020-03-08 10:26:10 +0800

    想复杂了

--- myrtle/date0308/edit_distance.py
+++ myrtle/date0308/edit_distance.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/edit-distance/
-# 神作，在 leetcode 里面 summit 跟 run 的结果不同，在本地
+# 神作，在 leetcode 里面 summit 跟 run 的结果不同！
 
 
 class Solution:
---
+++ myrtle/date0308/intersection_of_two_linked_list.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/intersection-of-two-linked-lists/
+# 想复杂了，先对齐然后比较就好了
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        return None
