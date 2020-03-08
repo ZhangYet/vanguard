2cf88e2874320f1dfeee917cf64852cbbe990d81
dantezy <zhangyet@gmail.com>
2020-03-08 09:47:26 +0800

    相通了也不难

---
+++ myrtle/date0308/edit_distance.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/edit-distance/
+
+
+class Solution:
+    cache = {}
+
+    def minDistance(self, word1: str, word2: str) -> int:
+        def edist(i, j) -> int:
+            if (i, j) in self.cache:
+                return self.cache[(i, j)]
+
+            if i == len(word1):
+                return max(0, len(word2) - j)
+
+            if j == len(word2):
+                return max(0, len(word1) - i)
+
+            if word1[i] == word2[j]:
+                d = edist(i + 1, j + 1)
+                self.cache[(i, j)] = d
+                return d
+            else:
+                d_replace = edist(i + 1, j + 1) + 1
+                d_delete = edist(i + 1, j) + 1
+                d_insert = edist(i, j + 1) + 1
+                res = min(d_replace, d_delete, d_insert)
+                self.cache[(i, j)] = res
+                return res
+
+        return edist(0, 0)
+
+
+def test_case(word1: str, word2: str) -> int:
+    s = Solution()
+    return s.minDistance(word1, word2)
