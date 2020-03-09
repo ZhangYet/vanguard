fb5fa16f58969081eaec347baaf111b490d956c1
zhangye <zhangyet@gmail.com>
2020-03-09 21:41:11 +0800

    没啥改进

---
+++ myrtle/date0309/find_k_pairs_with_smallest_sums.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
+from typing import List
+class Solution:
+    def kSmallestPairs(
+        self, nums1: List[int], nums2: List[int], k: int
+    ) -> List[List[int]]:
+        import heapq
+        data = []
+
+        head_1, head_2 = 0, 0
+        for _ in range(k):
+            if nums1[head_1] < nums2[head_2]:
+                for y in nums2[head_2:]:
+                    heapq.heappush(data, [nums1[head_1], y])
+                head_1 += 1
+            else:
+                for x in nums1[head_1:]:
+                    heapq.heappush(data, [x, nums2[head_2]])
+                head_2 += 1
+        return heapq.nsmallest(k, data, key=sum)
+                
