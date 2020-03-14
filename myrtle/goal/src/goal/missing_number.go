453517b9fe0647b65139db301091013e3f56ef6a
dantezy <zhangyet@gmail.com>
2020-03-15 00:14:31 +0800

    用高斯求和公式是最快的了

--- myrtle/goal/src/goal/missing_number.go
+++ myrtle/goal/src/goal/missing_number.go
@@ -1,38 +1,13 @@
 package goal
 
-import (
-	"fmt"
-	"math"
-)
-
 // https://leetcode.com/problems/missing-number/
-
-func abs(i int) int {
-	if i >= 0 {
-		return i
-	}
-	return -i
-}
 
 func missingNumber(nums []int) int {
 	l := len(nums)
-	nums = append(nums, math.MaxInt32)
-	for _, num := range nums[:l] {
-		index := abs(num)
-		if index == math.MaxInt32 {
-			index = 0
-		}
-		if nums[index] > 0 {
-			nums[index] = -nums[index]
-		}
-		if nums[index] == 0 {
-			nums[index] = -math.MaxInt32
-		}
+	total := (l + 1) * l / 2
+	pSum := 0
+	for _, n := range nums {
+		pSum += n
 	}
-	for k, v := range nums {
-		if v >= 0 {
-			return k
-		}
-	}
-	return 0
+	return total - pSum
 }
--- myrtle/goal/src/goal/missing_number_test.go
+++ myrtle/goal/src/goal/missing_number_test.go
@@ -11,34 +11,34 @@ func Test_missingNumber(t *testing.T) {
 		args args
 		want int
 	}{
-		//{
-		//	name: "case1",
-		//	args: args{
-		//		nums: []int{3, 0, 1},
-		//	},
-		//	want: 2,
-		//},
-		//{
-		//	name: "case2",
-		//	args: args{
-		//		nums: []int{3, 2, 1},
-		//	},
-		//	want: 0,
-		//},
-		//{
-		//	name: "case3",
-		//	args: args{
-		//		nums: []int{0},
-		//	},
-		//	want: 1,
-		//},
-		//{
-		//	name: "case4",
-		//	args: args{
-		//		nums: []int{9, 6, 4, 2, 3, 5, 7, 0, 1},
-		//	},
-		//	want: 8,
-		//},
+		{
+			name: "case1",
+			args: args{
+				nums: []int{3, 0, 1},
+			},
+			want: 2,
+		},
+		{
+			name: "case2",
+			args: args{
+				nums: []int{3, 2, 1},
+			},
+			want: 0,
+		},
+		{
+			name: "case3",
+			args: args{
+				nums: []int{0},
+			},
+			want: 1,
+		},
+		{
+			name: "case4",
+			args: args{
+				nums: []int{9, 6, 4, 2, 3, 5, 7, 0, 1},
+			},
+			want: 8,
+		},
 		{
 			name: "case5",
 			args: args{
