48dac3b29918491fefe115ab4cb535880f40ece9
dantezy <zhangyet@gmail.com>
2020-03-19 20:56:18 +0800

    仔细想想就好

---
+++ myrtle/goal/src/goal/univalued_binary_tree.go
@@ -0,0 +1,16 @@
+package goal
+
+// 虽然是递归，但是也没有那么简单，其实没一层子树都得比较
+
+func isUnivalTree(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if root.Left != nil && root.Val != root.Left.Val {
+		return false
+	}
+	if root.Right != nil && root.Val != root.Right.Val {
+		return false
+	}
+	return isUnivalTree(root.Left) && isUnivalTree(root.Right)
+}
