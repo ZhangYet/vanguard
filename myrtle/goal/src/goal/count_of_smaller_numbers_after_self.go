72aed831458a405083c1bd2caf73c9a0f490838d
dantezy <zhangyet@gmail.com>
2020-04-04 17:24:46 +0800

    靠 go 的性能肛过去了。

--- myrtle/goal/src/goal/count_of_smaller_numbers_after_self.go
+++ myrtle/goal/src/goal/count_of_smaller_numbers_after_self.go
@@ -2,35 +2,25 @@ package goal
 
 // https://leetcode.com/problems/count-of-smaller-numbers-after-self/
 
-func countSmaller(nums []int) []int {
-	stack := make([]int, 0)
-	res := make([]int, len(nums))
-	for idx := len(nums) - 1; idx >= 0; idx-- {
-		if len(stack) == 0 {
-			res[idx] = 0
-			stack = append(stack, nums[idx])
-			continue
+func countsmaller(nums []int, target int) int {
+	res := 0
+	for _, num := range nums {
+		if num < target {
+			res += 1
 		}
-		if stack[len(stack)-1] < nums[idx] {
-			res[idx] = stack[len(stack)-1]
-			stack = append(stack, nums[idx])
-			continue
-		}
-		for {
-			top := len(stack) - 1
-			if top < 0 || stack[top] < nums[idx] {
-				if top == -1 {
-					res[idx] = 0
-					stack = append(stack, nums[idx])
-					break
-				}
-				res[idx] = stack[top]
-				stack = append(stack, nums[idx])
-				break
-			}
-			stack = stack[:top]
-		}
+	}
+	return res
+}
 
+func countSmaller(nums []int) []int {
+	if len(nums) <= 0 {
+		return []int{}
+	}
+	res := make([]int, len(nums))
+	end := len(nums) - 1
+	res[end] = 0
+	for idx := len(nums) - 2; idx >= 0; idx-- {
+		res[idx] = countsmaller(nums[idx:], nums[idx])
 	}
 	return res
 }
