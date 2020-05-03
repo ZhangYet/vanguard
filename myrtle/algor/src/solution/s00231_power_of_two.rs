0fdd4d67d97e33a9cbf16fb11eeaea7acb81e5cd
dante zhang <zhangye@16financial.com>
2020-05-04 03:28:43 +0800

    还行

--- myrtle/algor/src/solution/s00231_power_of_two.rs
+++ myrtle/algor/src/solution/s00231_power_of_two.rs
@@ -2,13 +2,16 @@ pub struct Solution {}
 
 impl Solution {
     pub fn is_power_of_two(n: i32) -> bool {
-	if n == 1 {
-	    return true;
-	}
-	if n % 2 == 1 {
-	    return false;
-	}
-	return  Self::is_power_of_two(n / 2);
+        if n == 0 {
+            return false;
+        }
+        if n == 1 {
+            return true;
+        }
+        if n % 2 == 1 {
+            return false;
+        }
+        return Self::is_power_of_two(n / 2);
     }
 }
 
@@ -18,7 +21,8 @@ mod tests {
 
     #[test]
     fn test_is_power_of_two() {
-	assert_eq!(true, Solution::is_power_of_two(1));
-	assert_eq!(false, Solution::is_power_of_two(3));
+        assert_eq!(false, Solution::is_power_of_two(0));
+        assert_eq!(true, Solution::is_power_of_two(1));
+        assert_eq!(false, Solution::is_power_of_two(3));
     }
 }
