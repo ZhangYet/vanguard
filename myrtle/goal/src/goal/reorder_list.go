5ca4a4eb8c56e1350ea8d4944f9d2398c4dfb9b7
dantezy <zhangyet@gmail.com>
2021-01-04 14:48:05 +0800

    超慢的递归版本

---
+++ myrtle/goal/src/goal/reorder_list.go
@@ -0,0 +1,39 @@
+package goal
+
+func reorderList(head *ListNode) {
+	cur := head
+	if cur == nil {
+		return
+	}
+	if cur.Next == nil {
+		return
+	}
+	reorderListHead(cur)
+	cur = cur.Next.Next
+	for cur != nil {
+		reorderListHead(cur)
+		if cur.Next == nil {
+			break
+		}
+		cur = cur.Next.Next
+	}
+
+	return
+}
+func reorderListHead(head *ListNode) {
+	if head.Next == nil {
+		return
+	}
+	beforeTail, tail := head, head.Next
+	if tail.Next == nil {
+		return // 只有两个节点的平凡情况
+	}
+	for tail.Next != nil {
+		tail = tail.Next
+		beforeTail = beforeTail.Next
+	}
+	beforeTail.Next = nil
+	tail.Next = head.Next
+	head.Next = tail
+	return
+}
